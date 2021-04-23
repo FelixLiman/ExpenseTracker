6ec0e1b3b2e7a525311c2d6412eb05ab2f44be1c
Felix Liman <felixlim75@gmail.com>
2021-04-23 14:46:28 +0700

    Initial Commit

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by Felix Liman on 23/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
